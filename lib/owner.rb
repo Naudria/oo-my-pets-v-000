c68dad2a4d4ca9905abb0d1db56a2939741061f2
Rima Jean <rima.jean@gmail.com>
2018-03-12 22:51:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,7 +27,18 @@ class Owner
 
   def buy_fish(name)
     fish = Fish.new(name)
-    self.pets[:cats] << cat 
+    self.pets[:cats] << cat
   end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    self.pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    self.pets[:dogs] << dog
+  end
+
 
 end
