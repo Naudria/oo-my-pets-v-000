207e286903216717cc34823d017dacb60e0d9321
Rima Jean <rima.jean@gmail.com>
2018-03-12 22:39:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,5 +2,10 @@ class Owner
   attr_accessor :name, :pets
   attr_reader :species
 
-  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
 end
